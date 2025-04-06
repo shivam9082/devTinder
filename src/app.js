595f99763015b9f8c3ce9510944fe4f0b60949f3
Shivam Pandey <shpandey9082@gmail.com>
2025-04-06 16:46:25 +0530

    got to know about differenct http methods

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 
-//function inside app.use() is known as request handler..
+//function inside app.use() is known as route handler..
+app.get("/user",function(req,res){
+ res.send("user get method is called successfully.");
+});
 
+app.post("/user",function(req,res){
+    res.send("user post method is called successfully.");
+   });
 
+   app.delete("/user",function(req,res){
+    res.send("user delete method is called successfully.");
+   });
 app.use("/",function(req,res){
     res.send('server is listening on port 3000 on route /');
 });
