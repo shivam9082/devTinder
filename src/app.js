88154ec05b21597e804d88c70751edfb5671f0d2
Shivam Pandey <shpandey9082@gmail.com>
2025-04-06 23:37:28 +0530

    wild-card error handling

--- src/app.js
+++ src/app.js
@@ -24,22 +24,6 @@ app.get("/admin/deleteUser",(req,res)=>{
 });
 
 
-// app.get("/user",(req,res,next)=>{
-// console.log('response1');
-// next();
-// },[(req,res,next)=>{
-//     console.log('response2');
-//     next();
-// },(req,res,next)=>{
-//     console.log('response3');
-//     next();
-// }],
-// (req,res)=>{
-//     console.log('resp4');
-//     res.send('hello client');
-// }
-// );
-
 
 
 
