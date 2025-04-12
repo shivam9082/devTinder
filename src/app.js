189de29c17ec5fdc923f6f20d335cfe8f6774996
Shivam Pandey <shpandey9082@gmail.com>
2025-04-12 13:16:55 +0530

    getting data from database and sending it to user

--- src/app.js
+++ src/app.js
@@ -6,6 +6,37 @@ const User = require("./models/user");
 // middleware provided by the express
 app.use(express.json());
 
+//getting an user from the database using condition
+app.get("/user", async (req,res)=>{
+    const email = req.body.emailId;
+    try{
+        const users = await User.find({emailId:email});
+        if(users.length ===0){
+            res.send("User not found");
+        }else{
+            res.send(users);
+        }
+    }catch(err){
+        res.status(400).send("something went wrong")
+    }
+});
+
+//getting all users.. as feed
+app.get("/feed",async (req,res)=>{
+
+    try{
+        const users = await User.find({});
+        if(users.length === 0){
+            res.send("No user present at the moment");
+        }
+        else{
+            res.send(users);
+        }
+    }catch(err){
+        res.status(400).send("something went wrong");
+    }
+});
+
 app.post("/signup",async (req,res)=>{
     const user = new User(req.body);
     
