b65cb0133bbbc77d91b4d17aaa0a1aea68810740
Shivam Pandey <shpandey9082@gmail.com>
2025-04-12 23:40:57 +0530

    adding validation to our database

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ const User = require("./models/user");
 // middleware provided by the express
 app.use(express.json());
 
+//deleting user in the database
+
+app.delete("/user", async (req,res)=>{
+    const userId = req.body.userId;
+    try{
+        //const users = await User.findByIdAndDelete({_id:userId});
+        const users = await User.findByIdAndDelete(userId);
+        console.log(users);
+        res.send("User deleted successfully");
+    }catch(err){
+        res.status(400).send("something went wrong")
+    }
+});
+
+//updating user in the database
+app.patch("/user", async (req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+        const users = await User.findByIdAndUpdate({_id:userId},data,{
+            returnDocument: "after",
+            runValidatiors:true,
+        });
+        console.log(users);
+        res.send("User updated successfully");
+    }catch(err){
+        res.status(400).send("something went wrong")
+    }
+});
+
 //getting an user from the database using condition
 app.get("/user", async (req,res)=>{
     const email = req.body.emailId;
--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,52 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName : {type: String},
-    lastName : {type : String},
-    emailId : {type : String},
-    password :{type : String},
-    age : {type : Number},
-    gender : {type : String}
-});
+    firstName : {
+        type: String,
+        required: true,
+        minLength : 3,
+        maxLength : 50,
+    },
+    lastName : {
+        type : String
+    },
+    emailId : {
+        type : String,
+        required: true,
+        unique : true,
+        lowercase : true,
+        trim : true,
+    },
+    password :{
+        type : String,
+        required : true,
+    },
+    age : {
+        type : Number,
+        required : true,
+        min : 18
+    },
+    gender : {
+        type : String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender is not valid!!");
+            }
+        },
+    },
+    photoUrl : {
+        type : String,
+        default : "photo-url",
+    },
+    about : {
+        type : String,
+        default : "Hey this is about me",
+    },
+    skills : {
+        type : [String],
+    }
+    
+}, {timestamps:true});
 
 const User = mongoose.model("User",userSchema);
 
